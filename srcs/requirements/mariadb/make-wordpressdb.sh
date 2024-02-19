2a962f74e33dc47660f8df5b38d73272467c7a22
rnakaa <102458195+rnakaa@users.noreply.github.com>
2024-02-20 02:40:40 +0900

    separate network

--- srcs/requirements/mariadb/make-wordpressdb.sh
+++ srcs/requirements/mariadb/make-wordpressdb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+service mariadb start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress_db;"
 mysql -u root -e "CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'user'@'%';"
