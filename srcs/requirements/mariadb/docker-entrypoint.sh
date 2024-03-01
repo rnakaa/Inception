0c3deb40a1dcff413dc8a11a222ef041fc3099b5
rnakaa <102458195+rnakaa@users.noreply.github.com>
2024-03-01 14:20:42 +0900

    healty

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-apt-get install -y mariadb-server openrc
+apt-get install -y mariadb-server
 
-
-# mariadbd &
-# PID=$!
-# sleep 3
-declare -g MARIADB_PID
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-# kill $PID
-# wait $PID
 sed -i '/^bind-address/d' /etc/mysql/mariadb.conf.d/50-server.cnf
+# mkdir -p /run/mysqld
+# chown -R mysql:mysql /run/mysqld
+# "$@" &
+# MARIADB_PID=$!
+# sleep 5
+service mariadb start
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+service mariadb stop
+# kill "$MARIADB_PID"
+# wait "$MARIADB_PID"
+
 
 exec "$@"
